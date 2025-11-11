9920ecccd1fd00a0be191140486ce93b8cda658d
NITHISH B V <bvnithish2002@gmail.com>
2025-11-11 11:54:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
     updateActiveSection();
   });
 
+  // Smooth scrolling for nav links
   $(".header ul li a").click(function (e) {
     e.preventDefault();
     var target = $(this).attr("href");
@@ -26,26 +27,30 @@ $(document).ready(function () {
   ScrollReveal({ distance: "100px", duration: 2000, delay: 200 });
   ScrollReveal().reveal(".header a, .profile-photo, .about-content, .education", { origin: "left" });
   ScrollReveal().reveal(".header ul, .profile-text, .about-skills, .internship", { origin: "right" });
-  ScrollReveal().reveal(".project-title, .contact-title", { origin: "top" });
-  ScrollReveal().reveal(".projects, .contact", { origin: "bottom" });
+  ScrollReveal().reveal(".project-title, .skills-title, .contact-title", { origin: "top" });
+  ScrollReveal().reveal(".projects, .skills-grid, .contact", { origin: "bottom" });
 
-  // Mobile menu toggle fix for smoother and immediate response
+  // Mobile menu toggle
   $(".menu_icon").click(function () {
     $(".header ul").toggleClass("active");
   });
 
+  // Update active nav item on scroll
   function updateActiveSection() {
     var scrollPosition = $(window).scrollTop();
+
     if (scrollPosition === 0) {
       $(".header ul li a").removeClass("active");
       $(".header ul li a[href='#home']").addClass("active");
       return;
     }
+
     $("section").each(function () {
       var target = $(this).attr("id");
       var offset = $(this).offset().top;
       var height = $(this).outerHeight();
-      if (scrollPosition >= offset - 40 && scrollPosition < offset + height - 40) {
+
+      if (scrollPosition >= offset - 50 && scrollPosition < offset + height - 50) {
         $(".header ul li a").removeClass("active");
         $(".header ul li a[href='#" + target + "']").addClass("active");
       }
